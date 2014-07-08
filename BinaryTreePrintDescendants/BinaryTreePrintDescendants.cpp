3d6d1b99190020be25f8948a1c8a44d84f7127e5
Savio Figueiredo <saviofigueiredo@outlook.com>
2014-07-08 20:35:36 -0300

    Adding problem statement to binary tree print descendants and using template to define the node structure. Adding a new visual studio file to .gitignore

--- BinaryTreePrintDescendants/BinaryTreePrintDescendants.cpp
+++ BinaryTreePrintDescendants/BinaryTreePrintDescendants.cpp
@@ -1,225 +1,111 @@
+/*
+Given a Binary Tree and a key, write a function that prints all the descendants of the key in the given binary tree.
+
+For example, if the given tree is following Binary Tree and key is 2, then your function should print 4, 5 and 7.
+
+		1
+	  /   \
+     2      3
+	/  \
+   4    5
+  /
+ 7
+
+*/
 #include <iostream>
 #include <stack>
 #include <algorithm>
-typedef struct Node
+
+template <typename T>
+struct Node
 {
 	Node* left;
 	Node* right;
-	int data;
-}Node;
+	T data;
 
-void printDescendants(Node *root, int data, bool print)
-{
-	if (root == nullptr)
+	Node(T newData)
 	{
-		return;
-	}
-	
-	if (print == true)
-	{
-		std::cout << root->data << std::endl;
+		data = newData;
+		left = right = nullptr;
 	}
 
-	if (print == false && root->data == data)
+	void printDescendants(T nodeData, bool print)
 	{
-		print = true;
-	}
-
-	printDescendants(root->left, data, print);
-	printDescendants(root->right, data, print);
-}
-
-void printDescendants(Node* root, int data)
-{
-	bool elementFound = false;
-
-	std::stack<Node*> nodesToVisit;
-
-	nodesToVisit.push(root);
-
-	while (nodesToVisit.empty() == false)
-	{
-		Node* current = nodesToVisit.top();
-		nodesToVisit.pop();
-
-		if (current->data == data)
+		if (print == true)
 		{
-			while (nodesToVisit.empty() == false)
+			std::cout << data << std::endl;
+		}
+
+		if (print == false && data == nodeData)
+		{
+			print = true;
+		}
+
+		if (left != nullptr)
+		{
+			left->printDescendants(nodeData, print);
+		}
+		if (right != nullptr)
+		{
+			right->printDescendants(nodeData, print);
+		}
+	}
+
+	void printDescendants(int nodeData)
+	{
+		bool elementFound = false;
+
+		std::stack<Node*> nodesToVisit;
+		nodesToVisit.push(this);
+
+		while (nodesToVisit.empty() == false)
+		{
+			auto current = nodesToVisit.top();
+			nodesToVisit.pop();
+
+			if (current->data == nodeData)
 			{
-				nodesToVisit.pop();
+				while (nodesToVisit.empty() == false)
+				{
+					nodesToVisit.pop();
+				}
+
+				elementFound = true;
+			}
+			else if (elementFound == true)
+			{
+				std::cout << current->data << std::endl;
 			}
 
-			elementFound = true;
-		}
-		else if (elementFound == true)
-		{
-			std::cout << current->data << std::endl;
-		}
-
-		if (current->left != nullptr)
-		{
-			nodesToVisit.push(current->left);
-		}
-		if (current->right != nullptr)
-		{
-			nodesToVisit.push(current->right);
-		}
-	}
-}
-
-bool printAncestors(Node* root, int data)
-{
-	if (root == nullptr)
-	{
-		return false;
-	}
-	else if (root->data == data)
-	{
-		return true;
-	}
-	else if (printAncestors(root->left, data) || printAncestors(root->right, data))
-	{
-		std::cout << root->data << std::endl;
-		return true;
-	}
-
-	return false;
-}
-
-int numberOfDescendants(Node* root)
-{
-	if (root == nullptr)
-	{
-		return 0;
-	}
-
-	int n = 0;
-
-	if (root->left != nullptr)
-	{
-		n += 1 + numberOfDescendants(root->left);
-	}
-
-	if (root->right != nullptr)
-	{
-		n += 1 + numberOfDescendants(root->right);
-	}
-
-	return n;
-}
-
-int numberOfDescendants(Node* node, int data)
-{
-	std::stack<Node*> nodes;
-
-	nodes.push(node);
-
-	bool elementFound = false;
-
-	int count = 0;
-
-	while (nodes.empty() == false)
-	{
-		auto currentNode = nodes.top();
-		nodes.pop();
-
-		if (elementFound == true)
-		{
-			count++;
-		}
-		else if (currentNode->data == data)
-		{
-			elementFound = true;
-
-			while (nodes.empty() == false)
+			if (current->left != nullptr)
 			{
-				nodes.pop();
+				nodesToVisit.push(current->left);
+			}
+			if (current->right != nullptr)
+			{
+				nodesToVisit.push(current->right);
 			}
 		}
-
-		if (currentNode->left != nullptr)
-		{
-			nodes.push(currentNode->left);
-		}
-		if (currentNode->right != nullptr)
-		{
-			nodes.push(currentNode->right);
-		}
 	}
+};
 
-	return count;
-}
-
-Node* newNode(int data)
-{
-	Node* node = new Node;
-	node->data = data;
-	node->left = node->right = nullptr;
-	return (node);
-}
-
-int printKDistancesFromLeaf(Node* root, int k)
-{
-	if (root == nullptr)
-	{
-		return -1;
-	}
-	if (root->left == nullptr && root->right == nullptr)
-	{
-		return 0;
-	}
-
-	int leftDistance = printKDistancesFromLeaf(root->left, k);
-
-	if (leftDistance != -1)
-	{
-		if (leftDistance + 1 == k)
-		{
-			std::cout << root->data << std::endl;
-		}
-
-		leftDistance += 1;
-	}
-
-	int rightDistance = printKDistancesFromLeaf(root->right, k);
-	if (rightDistance != -1)
-	{
-		if (rightDistance + 1 == k)
-		{
-			std::cout << root->data << std::endl;
-		}
-		rightDistance += 1;
-	}
-
-	return std::max(leftDistance, rightDistance);
-}
 
 int main(int argc, char* argv[])
 {
-	// Let us create binary tree given in the above example
-	Node * root = newNode(1);
-	root->left = newNode(2);
-	root->right = newNode(3);
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
-	root->right->left = newNode(6);
-	root->right->right = newNode(7);
-	root->right->left->right = newNode(8);
+	auto root = new Node<int>(1);
+	root->left = new Node<int>(2);
+	root->right = new Node<int>(3);
+	root->left->left = new Node<int>(4);
+	root->left->right = new Node<int>(5);
+	root->right->left = new Node<int>(6);
+	root->right->right = new Node<int>(7);
+	root->right->left->right = new Node<int>(8);
 
 	std::cout << "Descendants of node 2 are: " << std::endl;
-	printDescendants(root, 2, false);
+	root->printDescendants(2, false);
 
-	std::cout << "Ancestors of node 8 are: " << std::endl;
-	printAncestors(root, 8);
-
-	std::cout << "Number of descendantes of root are: " << std::endl;
-	std::cout << numberOfDescendants(root);
-
-	std::cout << "Number of descendantes of node 2 is: " << std::endl;
-	std::cout << numberOfDescendants(root, 2);
-
-	std::cout << "Number of nodes at distance 1 from leaf are: " << std::endl;
-	printKDistancesFromLeaf(root, 1);
+	std::cout << "Descendants of node 2 are: " << std::endl;
+	root->printDescendants(2);
 
 	return EXIT_SUCCESS;
 }
